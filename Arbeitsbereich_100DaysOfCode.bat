040a1003c9320f4a5dcb9232e550a0d49eaefff5
swaehnie <85112906+swaehnie@users.noreply.github.com>
2022-01-09 13:07:37 +0100

    Add files via upload

---
+++ Arbeitsbereich_100DaysOfCode.bat
@@ -0,0 +1,15 @@
+@echo off
+echo freecodecamp
+start "" https://www.freecodecamp.org/learn
+ping 127.0.0.1 -n 3
+echo github/swaehnie/100-day-of-code
+start "" https://github.com/swaehnie/100-days-of-code
+ping 127.0.0.1 -n 3
+echo twitter
+start "" https://twitter.com/notifications
+ping 127.0.0.1 -n 3
+echo pomofocus
+start "" https://pomofocus.io/
+ping 127.0.0.1 -n 3
+echo alles erledigt
+rem pause
